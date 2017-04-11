e86b5490d40dda4a7db4905a2a31c09974944be5
Alec Paulson <alec.paulson@vantiv.com>
2017-04-11 12:29:21 -0600

    API dev and README start

--- website/controllers/endAuction.js
+++ website/controllers/endAuction.js
@@ -4,14 +4,14 @@ var router = express.Router();
 router.post('/', function(req, res) {
   //user = req.user.id
   //text = req.body.text
-  console.log('registerPasscode POST hit');
+  console.log('endAuction POST hit');
   //Comment.create(user, text, function (err, comment) {
     res.redirect('/')
   //})
 })
 
 router.get('/:id', function(req, res) {
-  res.send("registerPasscode GET hit. id: " + req.params.id);
+  res.send("endAuction GET hit");
 })
 
 module.exports = router;
--- website/controllers/index.js
+++ website/controllers/index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/registerPasscode', require('./registerPasscode'));
+router.use('/endAuction', require('./endAuction'));
+router.use('/highestBid', require('./highestBid'));
 
 module.exports = router;
