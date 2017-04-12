6f035684607ecea850ad253f897258cb502ac6a7
agharris73 <agharris73@gmail.com>
2017-04-12 09:32:40 -0600

    website - serve static files .css

--- website/app.js
+++ website/app.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var path = require('path') // trying to figure out how to serve static files in express 
 var http = require('http').Server(app);
 var alexaPasscodes = require('./helpers/alexapasscodes.js');
@@ -12,9 +13,9 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
 app.use('/api', require('./controllers')(alexaPasscodes, vsocketio));
 
