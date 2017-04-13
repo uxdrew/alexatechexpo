6396109e8a0861791a24ddc8d9bfbaea90ff513a
Alec Paulson <alec.paulson@vantiv.com>
2017-04-13 14:02:26 -0600

    Added jade

--- website/app.js
+++ website/app.js
@@ -6,6 +6,10 @@ var alexaPasscodes = require('./helpers/alexapasscodes.js');
 var vsocketio = require('./socketio/vsocketio.js')(http, alexaPasscodes);
 var bodyParser = require('body-parser');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public'))); // trying to figure out how to serve static files in express
 
 var port = process.env.PORT || 8080; 
@@ -14,6 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', require('./controllers')(alexaPasscodes, vsocketio));
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
