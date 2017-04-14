37a53237433d96e9f9e6ec77b9a8146360607de2
Alec Paulson <alec.paulson@vantiv.com>
2017-04-13 20:02:21 -0600

    Moved to pug from jade. Added variable values

--- website/app.js
+++ website/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public'))); // trying to figure out how to serve static files in express
 
@@ -21,7 +21,16 @@ app.use('/api', require('./controllers')(alexaPasscodes, vsocketio));
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+  { 
+    title: 'Express', 
+    textJumbotronHeader: 'Well. Hello there highest bidder...',
+    textDescription: "Link your Alexa device for voice-first hotness!",
+    labelCurrentBid: "Current Bid",
+    labelBidPrice: "$100.00",
+    btnLabelBid: "Place Bid",
+    iconResponse: "Not Paired"
+  });
 });
 
 http.listen(port, function(){
