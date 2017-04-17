2a2cd7b835c1c5effa19c454b81a4991b38ddc75
Alec Paulson <alec.paulson@vantiv.com>
2017-04-17 12:56:22 -0600

    page notes

--- website/public/js/index.js
+++ website/public/js/index.js
@@ -1,5 +1,6 @@
 $(function () {
     var socket = io();
+    var lastbid = null;
     $('form').submit(function () {
         //console.log(socket.id);
         var bid = {
@@ -28,10 +29,47 @@ $(function () {
         $('#bid-text').val('');
         return false;
     });
+
+    $('linkform').submit(function() {
+        //make api call to /api/linkaccounts
+
+        //depending on successful pairing or not, 
+        //show returned clientid and hide form
+        //or do nothing
+    });
     socket.on('new bid', function (msg) {
         console.log(msg);
+        lastbid = msg;
         $('#bid-price').text("$" + msg.bid);
+
+        if(msg.socketid == socket.id) {
+            //make request to tripos with winning amount
+
+            //the bid is mine, i'm winning!!
+            //page changes here
+            //have view receipt button
+            //opens modal, displays tripos raw response (for starters), and link to express dashboard
+        }
+        else {
+            //bid isn't mine, i'm losing :(
+            //page changes here
+        }
     });
+
+    //auction has closed event here
+    //page changes to show auction has ended
+    socket.on('end auction', function (msg) {
+        console.log(msg);
+        if(lastbid.socketid == socket.id) {
+            //i won!
+            //page changes here
+        }
+        else {
+            //i lost!
+            //page changes here
+        }
+    });
+
     socket.on('connect', function () {
         console.log("Web Socket Connected - socketid: " + socket.id);
     });
